c9197f7939d8bb024f50957a73b2996e5748ba1e
Kevin Farid <kpelaez75@gmail.com>
2021-10-06 17:01:54 -0500

    :lipstick: [STYLE] reorganization the sass archives

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import Display from '../components/Display'
 
 export default function Home() {
   return (
-    <div className='indexPage'>
+    <div>
       <Display/>
-      <h2>MIS <span>PROYECTOS</span></h2>
-      <h2>MIS <span>ULTIMAS ENTRADAS</span></h2>
+      <h2>MIS <span className='text-primary-color'>PROYECTOS</span></h2>
+      <h2>MIS <span className='text-primary-color'>ULTIMAS ENTRADAS</span></h2>
     </div>
   )
 }
