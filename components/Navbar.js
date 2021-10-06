c515f048706edeb87ac4de4dd08041275049a163
Kevin Farid <kpelaez75@gmail.com>
2021-10-05 20:35:12 -0500

    :sparkles: [FEAT] add styles global and add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <Image
+          src="https://i.imgur.com/2IE7z1O.png"
+          alt="kevfarid_"
+          width={50}
+          height={59.76}
+        />
+      </div>
+      <div className='menu'>
+        <a className='select'>Inicio</a>
+        <a>Acerca</a>
+        <a>Proyectos</a>
+        <a>Blog</a>
+        <a>Concacto</a>
+        <a className='cv'>Descargar CV</a>
+      </div>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.scss'
+import Navbar from '../components/Navbar'
+import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container">
+      <Navbar/>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
