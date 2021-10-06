3f4cd4e88de404b9f8f4b4dce400cfb7f03e31f6
Kevin Farid <kpelaez75@gmail.com>
2021-10-06 12:48:16 -0500

    :lipstick: [FEAT] add styles for titles of the sections

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Display from '../components/Display'
 
 export default function Home() {
   return (
-    <div>
+    <div className='indexPage'>
       <Display/>
+      <h2>MIS <span>PROYECTOS</span></h2>
+      <h2>MIS <span>ULTIMAS ENTRADAS</span></h2>
     </div>
   )
 }
