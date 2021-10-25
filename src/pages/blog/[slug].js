3d0e5f3a202bd89f9e3624e12e0a66f0c63539e0
Kevin Farid <kpelaez75@gmail.com>
2021-10-24 20:39:48 -0500

    :recycle: [REFACTOR] the content web site to src folder

---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,36 @@
+import { getFileBySlug, getFiles } from '../../lib/mdx'
+import { MDXRemote } from "next-mdx-remote";
+
+export default function Post({ source, frontmatter }) {
+  return (
+    <MDXRemote {...source}/>
+  );
+}
+
+export async function getStaticPaths() {
+  const posts = await getFiles("blog");
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.replace(/\.mdx/, ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { source, frontmatter } = await getFileBySlug("blog", params.slug);
+
+  return {
+    props: {
+      source,
+      frontmatter: {
+        slug: params.slug,
+        ...frontmatter,
+      },
+    },
+  };
+}
